6e789be3b4c15f28cf4288838a50c5eb8620a7e4
Nate Rathbun <naterathbun@gmail.com>
2023-10-04 16:33:14 -0500

    added props to ListGroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><ListGroup /></div>;
+function App() {
+  let items = ["New York", "LA", "Paris", "Berlin", "Tokyo"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "LA", "Paris", "Berlin", "Tokyo"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -19,6 +24,7 @@ function ListGroup() {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
